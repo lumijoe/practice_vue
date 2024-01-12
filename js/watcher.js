0f6eec593fc71fa8355ec6191efda79576585dd6
lumijoe <spacedesign63@gmail.com>
2024-01-12 16:53:01 +0900

    lodash watcher

--- js/watcher.js
+++ js/watcher.js
@@ -1,15 +1,12 @@
-// js/watcher.js
-// list2-27(p45)0112
-
-let app = new Vue({
+new Vue({
     el: '#app',
     data: {
         name: '',      // 入力値
         upperName: ''  // 表示する値（大文字変換後の文字列）
     },
     created: function() {
-    //遅延処理用のdelayFuncメソッドを準備
-    this.delayFunc = _.debounce(this.getUpper, 2000); 
+        // 遅延処理用のdelayFuncメソッドを準備
+        this.delayFunc = _.debounce(this.getUpper, 2000); 
     },
     // nameプロパティが変化した時にdelayFuncメソッドを呼び出し
     watch: {
