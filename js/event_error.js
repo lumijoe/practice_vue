6283ae1d210563729ff565ab22f81159ef62f958
lumijoe <spacedesign63@gmail.com>
2024-01-22 15:44:57 +0900

    update

--- js/event_error.js
+++ js/event_error.js
@@ -4,7 +4,14 @@
 new Vue({
     el: '#app',
     data: {
-        result: ''
+        path: './images/wings.jpg'
+        // ,path2: './images/_wings.jpg'
+    },
+    methods: {
+        // 画像を読み込めない場合はエラー画像を表示
+        onerror: function() {
+            this.path = './images/noimage.jpg';
+        }
     }
 });
  
