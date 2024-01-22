8ab72fbdc579073c7ca6eb67ba0a7fa2f677ca2d
lumijoe <spacedesign63@gmail.com>
2024-01-22 15:57:26 +0900

    event click

---
+++ js/event_obj.js
@@ -0,0 +1,13 @@
+// list3-10(p61)0122
+// event_obj.js
+
+new Vue({
+    el: '#app',
+    methods: {
+        // クリック時にイベントオブジェクトをログに出力
+        onclick: function(e) {
+            console.log(e)
+        }
+    }
+});
+ 
