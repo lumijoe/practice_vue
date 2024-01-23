278a974999c4fc950dece2ef4ceba351f1251f19
lumijoe <spacedesign63@gmail.com>
2024-01-23 13:17:07 +0900

    fix file

--- js/model_check.js
+++ js/model_check.js
@@ -1,3 +1,20 @@
-// list3-24(p72)0123
-// model_check_multi.js
+// list3-22(p71)0122
+// model_check.js
 
+new Vue({
+    el: '#app',
+    data: {
+        messageDefault: '同意する',
+        agree: true
+    }
+});
+// new Vue({
+//     el: '#app',
+//     data: {
+//         agree: true,
+//         messageTrue: '同意する',
+//         messageFalse: '同意しない',
+//         messageDefault: '同意する'
+//     }
+// });
+ 
