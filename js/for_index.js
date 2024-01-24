2849e25ec07b63f229df5dba9f2bf1a765c80262
lumijoe <spacedesign63@gmail.com>
2024-01-24 16:47:06 +0900

    add image

--- js/for_index.js
+++ js/for_index.js
@@ -6,21 +6,25 @@ new Vue({
     data: {
         books: [
             {
+                image: 'images/noimage.jpg',
                 isbn: '978-4-7981-5757-3',
                 title: 'JavaScript逆引きレシピ',
                 price: 2800
             },
             {
+                image: 'images/_wings.jpg',
                 isbn: '978-4-8399-6644-7',
                 title: '楽しいラズパイ電子工作ブック',
                 price: 1900
             },
             {
+                image: 'images/noimage.jpg',
                 isbn: '978-4-7741-9763-0',
                 title: '3ステップでしっかり学ぶ Python入門',
                 price: 2480
             },
             {
+                image: 'images/_wings.jpg',
                 isbn: '978-4-7981-5382-7',
                 title: '独習C# 新版',
                 price: 3600
