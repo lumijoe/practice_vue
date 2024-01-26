a236be3173d72ccad6aa0f77e83a53b2e05c7887
lumijoe <spacedesign63@gmail.com>
2024-01-26 16:07:47 +0900

    v-html for no xss

---
+++ js/html.js
@@ -0,0 +1,12 @@
+// list3-73(p112)0126
+// html.js
+
+new Vue({
+    
+    el: '#app',
+    data: {
+        message: `<h3>WINGSプロジェクト</h3> 
+            <img src="https://www.web-deli.com/image/linkbanner_l.gif" alt="ロゴ" />`
+    }
+});
+
