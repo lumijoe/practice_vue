06defa62b479469547eef5e892fcc064fc921d6c
lumijoe <spacedesign63@gmail.com>
2024-01-26 15:35:34 +0900

    bind prop

---
+++ js/bind_obj.js
@@ -0,0 +1,16 @@
+// list3-67(p108)0126
+// bind_obj.js
+
+new Vue({
+    
+    el: '#app',
+    data: {
+        // 属性情報をまとめて定義
+        attrs: {
+            size: 20,
+            maxlength: 14,
+            required: true
+        }
+    }
+});
+
---
+++ js/bind_prop.js
@@ -0,0 +1,11 @@
+// list3-69(p109)0126
+// bind_prop.js
+
+new Vue({
+    
+    el: '#app',
+    data: {
+        text: '皆さん、こんにちは！'
+    }
+});
+
