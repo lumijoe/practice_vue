a821c07bd953fd0e6735f251f62e4cade7aeb01c
lumijoe <spacedesign63@gmail.com>
2024-01-26 13:14:13 +0900

    v-for focus

---
+++ js/for_multi.js
@@ -0,0 +1,32 @@
+// list3-61(p101)0126
+// for_multi.js
+
+new Vue({
+    
+    el: '#app',
+    data: {
+        songs: [
+            {
+                title: '赤とんぼ',
+                lyrics: '夕焼け小焼けの赤とんぼ...',
+                composer: '山田耕作'
+            },
+            {
+                title: '荒城の月',
+                lyrics: '春紅楼の花の宴 巡る盃影さして...',
+                composer: '瀧廉太郎'
+            },
+            {
+                title: 'どんぐりころころ',
+                lyrics: 'どんぐりころころ どんぶりこ...',
+                composer: '梁田貞'
+            },
+            {
+                title: '七つの子',
+                lyrics: '烏 なぜ啼くの 烏は山に...',
+                composer: '本居長世'
+            }
+        ]
+    }   
+});
+
