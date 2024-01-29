b227fd44c68f24ea3a76067840e7fa5e833b814e
lumijoe <spacedesign63@gmail.com>
2024-01-29 18:56:31 +0900

    mousebtn

--- js/event_mousebtn.js
+++ js/event_mousebtn.js
@@ -1,8 +1,31 @@
 // list3-104(p139)0129
-// event_mouesbtn.js
+// event_mousebtn.js
 
 new Vue({
-    el: '#app'
-    
+    el: '#app',
+    data: {
+        help: 'ヘルプ',
+        // コンテキストメニューの表示
+        pos: {
+            left: 0,
+            top: 0
+        },
+        // コンテキストメニューの表示状態
+        show: false
+    },
+    methods: {
+        // 左クリックでメニューを非表示
+        onleftclick: function() {
+            this.show = false;
+        },
+        // 右クリックでメニューを表示
+        onrightclick: function(e) {
+            this.pos = {
+                top: e.pageY + 'px',
+                left: e.pageX + 'px'
+            };
+            this.show = true;
+        }
+    }
 });
 
