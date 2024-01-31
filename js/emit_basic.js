e9435c2e4931f23e04684f36787b33857c9ee1f7
lumijoe <spacedesign63@gmail.com>
2024-01-31 14:16:17 +0900

    emit

--- js/emit_basic.js
+++ js/emit_basic.js
@@ -1,7 +1,19 @@
 // list4-12(p159)0130
 // emit_basic.js(修正版)
 
-
+Vue.component('my-counter', {
+    // ボタンクリックで加算する値
+    props: [ step ],
+    // クリック時にカウントアップ処理を実行
+    template: `<button type="button" v-on:click="onclick">
+        {{ step }}</button>`,
+    methods: {
+        // クリック時にイベントを発生
+        onclick:function() {
+            this.$emit('plus', Number(this.step));
+        }
+    }
+});
 
 new Vue({
     el: '#app'
