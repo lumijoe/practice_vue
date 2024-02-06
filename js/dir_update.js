97fa13cd09f95c0a55349db3ca63bab348909d39
lumijoe <spacedesign63@gmail.com>
2024-02-06 16:38:13 +0900

    add color

--- js/dir_update.js
+++ js/dir_update.js
@@ -3,3 +3,10 @@
 Vue.directive('highlight', function(el, binding, vnode, oldVnode) {
   el.style.backgroundColor = binding.value;
 });
+
+new Vue({
+  el: '#app',
+  data: {
+    color: 'yellow'
+  }
+});
