af270288776fb0e34f421b44dc30d62ced7ebfed
lumijoe <spacedesign63@gmail.com>
2024-02-06 16:37:35 +0900

    fix

--- js/dir_update.js
+++ js/dir_update.js
@@ -1,10 +1,5 @@
 
 
-Vue.directive('highlight', {
-  bind: function(el, binding, vnode, oldVnode) {
-   el.style.backgroundColor = binding.value;
- },
- update: function(el, binding, vnode, oldVnode) {
-   el.style.backgroundColor = binding.value;
- }
+Vue.directive('highlight', function(el, binding, vnode, oldVnode) {
+  el.style.backgroundColor = binding.value;
 });
